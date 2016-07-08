c0c6a11a76697c63caae4b89c10ce2e472046579
Christoffer <christoffer.karlberg@gmail.com>
2016-07-08 14:59:13 +0200

    Jag har ändrat

---
+++ Projectgroup2/index.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Projectgroup2
+{
+    public partial class index : System.Web.UI.Page
+    {
+        static string conStr = "Data Source=(local);Initial Catalog=Contacts; Integrated Security =SSPI;";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
